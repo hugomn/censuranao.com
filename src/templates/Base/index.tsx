0243b46b3c0eadc0e7a7d1bdd980a4c0503201a5
Hugo Nogueira <hugomn@gmail.com>
2020-06-07 14:52:07 +0200

    Add analytics

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { initGA, logPageView } from "lib/ga" 
 
 const BaseTemplate: React.FC = ({ children }) => {
+  useEffect(() => {
+    if (!(window as any).GA_INITIALIZED) {
+      initGA();
+      (window as any).GA_INITIALIZED = true;
+    }
+    logPageView() 
+  }, [])
+  
   return (
     <main data-testid="main">
       {children}
