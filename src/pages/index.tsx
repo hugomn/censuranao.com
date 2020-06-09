7a48fa9208bfc709acac32a36f7c99fc873ff2e4
Hugo Nogueira <hugomn@gmail.com>
2020-06-09 15:18:04 +0200

    Add viewport metatag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,6 +48,9 @@ const IndexPage: React.FC<IndexPageProps> = ({ data }) => {
 
   return (
     <BaseTemplate>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      </Head>
       <NextSeo
         title="Coronavírus Brasil - Censura Não!"
         description={description}
