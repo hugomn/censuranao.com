966e354249cee69c808d1ad18337ac232162b70c
Hugo Nogueira <hugomn@gmail.com>
2020-06-15 13:29:20 +0200

    Update 14/06/2020

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -99,7 +99,7 @@ const IndexPage: React.FC<IndexPageProps> = ({ data }) => {
         <div className={clsx("mt-24 md:mt-20", styles.welcome)}>
           <div className={styles.subtitle}>COVID19</div>
           <div className={styles.title}><b>Painel</b> Coronavírus</div>
-          <div className={styles.updated}>Atualizado em: 13/06/2020 18:40</div>
+          <div className={styles.updated}>Atualizado em: 14/06/2020 18:20</div>
         </div>
 
         <div className="flex flex-col md:flex-row ">
