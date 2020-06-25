5e2ec88c13cc7c9ea0b00244cadf836663033571
Hugo Nogueira <hugomn@gmail.com>
2020-06-25 22:25:52 +0200

    Add logging

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 export default async function test(req, res) {
   try {
-    axios.post(
+    console.log("testing deploy");
+    const response = await axios.post(
       "https://api.zeit.co/v1/integrations/deploy/QmcxbXzxbUu8dP22WCs4nJtd1pLcetqZqhoKLtaVTEyMjt/h8HQqpH4Y7"
     );
+    console.log(response);
     res.status(200).send("Success!");
   } catch (error) {
     res.status(400).send(error.message);
